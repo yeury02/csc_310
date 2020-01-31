8efeb271edf404e78aa0302614dd02143ffb03ee
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-01-30 22:52:44 -0500

    func from slides

--- conway_game.py
+++ conway_game.py
@@ -1,1 +1,20 @@
 import os 
+import time
+
+def display_array(ar):
+    "clear the screen, display the contents of an array, wait for 1sec"
+    os.system("clear")
+
+    rows = len(ar)  # grab the rows
+
+    if rows == 0:
+        raise ValueError("Array contains no data")
+
+    cols = len(ar[0])   # grab the columns - indices start at 0!
+
+    for i in range(rows):
+        for j in range(cols):
+            print(ar[i][j], end=' ')    # no carriage return, space separated
+        print()
+
+    time.sleep(1)
