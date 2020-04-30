c2e89cc5f0c8865ae7e7ac64465bc5edf0e10f19
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-04-29 21:38:35 -0700

    working

--- final_project/stock_prediction/app.py
+++ final_project/stock_prediction/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!!'
+def index():
+    return render_template('home.html')
+
+# @app.route('/home')
+# def home():
+#     return render_template('')
 
 if __name__ == "__main__":
     app.run(debug=True)
