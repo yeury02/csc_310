bba5097cde3ae922513d8b5398dadada69aa4f91
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-05-01 01:53:36 -0400

    some front end code done

--- final_project/stock_prediction/app.py
+++ final_project/stock_prediction/app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/home')
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
