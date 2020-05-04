dfcf4548b8cfcd3ee0badc86c66e08425a1a1691
Phidias Mendez <mendezphidias@gmail.com>
2020-05-04 19:08:23 -0400

    Returned a pic of full prediction

--- final_project/stock_prediction/app.py
+++ final_project/stock_prediction/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+import io
+from flask import Flask, render_template, Response
 from model import Predict
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
 
 app = Flask(__name__)
 
@@ -10,7 +13,12 @@ def index():
 
 @app.route('/plot/<stock>/<interval>/<hidden>/<iterations>/<tolerance>')
 def plot(stock, interval, hidden, iterations, tolerance):
-    
+    data = Predict(stock, interval, int(hidden), int(iterations), float(tolerance))
+
+    figure = data.predictionGraph()
+    output = io.BytesIO()
+    FigureCanvas(figure).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- final_project/stock_prediction/testing/model.py
+++ final_project/stock_prediction/testing/model.py
@@ -55,7 +55,7 @@ class Predict(object):
         axis = fig.add_subplot()
         axis.plot(dates, prices)
         axis.plot(dates, pricePredict, c='#5aa9ab')
-        return fig
+        return axis
     
 
     
