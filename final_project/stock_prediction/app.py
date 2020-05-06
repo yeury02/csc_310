c3dddb22f3f055d3eba656b5c3c7b52806191c11
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-05-05 20:40:53 -0400

    collected data from stock prediction page

--- final_project/stock_prediction/app.py
+++ final_project/stock_prediction/app.py
@@ -1,14 +1,23 @@
 import io
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, redirect, url_for
 from model import Predict
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/home', methods=["GET", "POST"])
 @app.route('/')
 def index():
+    if request.method == "POST":
+        ticker = request.form.get("ticker")
+        interval = request.form.get("interval")
+        iterations = request.form.get("iterations")
+        tolerance = request.form.get("tolerance")
+        layers = request.form.get("layers")
+
+        return redirect(url_for('post/+ /ticker + /interval + /iterations + /tolerance + /layers'))
+
     return render_template('home.html')
 
 @app.route('/plot/<stock>/<interval>/<hidden>/<iterations>/<tolerance>')
