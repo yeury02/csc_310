a1b25907a822b205708183b1f83d7f9893806a82
Phidias Mendez <mendezphidias@gmail.com>
2020-05-05 21:16:57 -0400

    Post Finalizations

--- final_project/stock_prediction/app.py
+++ final_project/stock_prediction/app.py
@@ -6,28 +6,28 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 app = Flask(__name__)
 
-@app.route('/home', methods=["GET", "POST"])
+@app.route('/home')
 @app.route('/')
 def index():
+    return render_template('home.html')
+
+@app.route('/plot', methods=["GET", "POST"])
+def plot():
     if request.method == "POST":
-        ticker = request.form.get("ticker")
+        stock = request.form.get("ticker")
         interval = request.form.get("interval")
         iterations = request.form.get("iterations")
         tolerance = request.form.get("tolerance")
-        layers = request.form.get("layers")
+        hidden = request.form.get("layers")
+    
+        data = Predict(stock, interval, int(hidden), int(iterations), float(tolerance))
 
-        return redirect(url_for('post/+ /ticker + /interval + /iterations + /tolerance + /layers'))
-
-    return render_template('home.html')
-
-@app.route('/plot/<stock>/<interval>/<hidden>/<iterations>/<tolerance>')
-def plot(stock, interval, hidden, iterations, tolerance):
-    data = Predict(stock, interval, int(hidden), int(iterations), float(tolerance))
-
-    figure = data.predictionGraph()
-    output = io.BytesIO()
-    FigureCanvas(figure).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
+        figure = data.predictionGraph()
+        output = io.BytesIO()
+        FigureCanvas(figure).print_png(output)
+        return Response(output.getvalue(), mimetype='image/png')
+    if request.method == "GET":
+        return redirect(url_for('home'))
 
 
 
